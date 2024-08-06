3d8233b88d2307a1651798857f6d5c355fea51a6
LUIZ HENRIQUE VIDAL ARAUJO <23111746@dsn127m1246734.cfp127aluno.local>
2024-08-06 17:28:28 -0300

    17:28-06/08/2024

--- ppdm/IMC-Calc-Vidal/composeApp/src/commonMain/kotlin/br/senai/sp/jandira/imc/ui/CalculadoraScreen.kt
+++ ppdm/IMC-Calc-Vidal/composeApp/src/commonMain/kotlin/br/senai/sp/jandira/imc/ui/CalculadoraScreen.kt
@@ -4,15 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,6 +26,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import imc_calc_vidal.composeapp.generated.resources.Res
@@ -28,6 +35,15 @@ import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun calculadoraScreen() {
+
+    var peso = remember {
+        mutableStateOf("")
+    }
+    var altura = remember {
+        mutableStateOf("")
+    }
+    var showContent = remember { mutableStateOf(false) }
+
     Surface(
         modifier = Modifier.fillMaxSize()
     ){
@@ -38,17 +54,17 @@ fun calculadoraScreen() {
                     .height(200.dp)
                     .fillMaxWidth()
                     .background(
-                        color = Color(0xcc000000)
+                        color = Color(0x00000000)
                     ),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceEvenly
             ){
                 Image(
                     modifier = Modifier
-                        .height(72.dp)
-                        .width(72.dp)
+                        .height(100.dp)
+                        .width(100.dp)
                         .background(
-                            color = Color(0xcc000000)
+                            color = Color(0x00000000)
                         ),
                     painter = painterResource(Res.drawable.bmi),
                     contentDescription = "",
@@ -58,21 +74,65 @@ fun calculadoraScreen() {
                 Text(
                     modifier = Modifier
                         .background(
-                            color = Color(0xcc000000)
+                            color = Color(0x00000000)
                         ),
-                    color = Color(0xffffffffff),
+                    color = Color(0xff000000),
                     fontFamily = FontFamily.Default,
-                    fontSize = 16.sp,
+                    fontSize = 24.sp,
                     fontStyle = FontStyle.Normal,
                     fontWeight = FontWeight.Bold,
                     text = "Calculadora IMC")
             }
-            Column {
+            Column (
+                modifier = Modifier
+                    .padding(10.dp)
+                    .height(200.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ){
+                OutlinedTextField(
+                    value = peso.value,
+                    onValueChange = {
+                        peso.value = it
+                                    },
+                    label = {
+                        Text("Peso (kg)")
+                            },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
 
+                Spacer(modifier= Modifier.height(8.dp))
+
+                OutlinedTextField(
+                    value = altura.value,
+                    onValueChange = {
+                        altura.value = it
+                                    },
+                    label = { Text("Altura (cm)") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+
+                Button(
+                    onClick = {
+                        calcularIMC(peso.value, altura.value)
+                        showContent = !showContent
+                    }
+                ){
+                    Text(
+                        text = "Calcular IMC!"
+                    )
+                }
             }
             Column {
 
             }
         }
     }
+}
+
+fun calcularIMC(peso: String, altura: String): String{
+    val imc = peso.toFloat()/(altura.toFloat()*altura.toFloat())
+
+    return imc.toString()
 }
